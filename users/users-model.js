3d9a1ceca4f4108c7c688efefbf8c6f180424dfc
Elizabeth Doyle <lizdoyle1121@gmail.com>
2019-12-12 22:59:30 -0500

    finshed model

--- users/users-model.js
+++ users/users-model.js
@@ -11,13 +11,15 @@ function find() {
     return db('users').select('id', 'username', 'password')
 }
 
-function findBy() {
-
+function findBy(filter) {
+    return db('users')
+        .where(filter)
 }
 
 async function add(user) {
+    const [id] = await db('users').insert(user)
 
-
+    return findById(id)
 }
 
 function findById(id) {
